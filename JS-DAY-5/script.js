dbe00a6b95df0f334a0dbc1c480c58f274f713b7
udayvijay <udayvijay08@gmail.com>
2026-01-21 10:40:22 +0530

    js-day-5

---
+++ JS-DAY-5/script.js
@@ -0,0 +1,42 @@
+//  slice
+
+// let str = "JavaScript is fun!";
+// let sliced = str.slice(0, 10);
+// console.log(sliced)
+
+
+
+// substring
+
+// let str = "JavaScript is amazing!";
+// let sub = str.substring(11, 17);
+// console.log(sub)
+
+
+
+
+// indexOf()
+
+// const paragraph = 'The quick brown fox jumps over the lazy dog. If the dog barked, was it really lazy?';
+// const indexOfFirst = paragraph.indexOf("dog");
+// console.log(indexOfFirst);
+
+
+// let firstname = "john";
+// let lastname = "Doe";
+// let fullname = firstname.concat(" ", lastname);
+// let fullname = firstname+" "+lastname
+
+// string literals
+
+// let fullname= `${firstname}: this is my firstname, ${lastname}:this is my last name`
+// console.log(fullname);
+
+// let university = "K.R Mangalam";
+// let program ="BCA AI/DS";
+// console.log(`my name is ${firstname}, I'm pursuing ${program} from ${university}`);
+
+let arr1=[1,2,3]
+let arr2=[4,5,6]
+let newarray = [...arr1,...arr2];
+console.log(newarray)
